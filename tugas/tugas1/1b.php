95ec44a3f9d6a025083097e7af268c6eaa9d2be7
dhikaid <bhadrika.223040018@mail.unpas.ac.id>
2023-02-21 14:16:01 +0700

    Memperbaiki soal 1b.php yang salah

--- tugas/tugas1/1b.php
+++ tugas/tugas1/1b.php
@@ -6,8 +6,9 @@ $npm = 18;
 ?>
 
 <!-- result -->
+<!-- Kalo eror hitung didalam kurung ($variable = $var + $var) -->
 <p>Aku adalah angka <b><?= $npm; ?></b></p>
-<p>Jika aku dikali 5, maka aku sekarang menjadi <b><?= $npm * 5; ?></b></p>
-<p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?= $npm / 2; ?></b></p>
-<p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?= $npm + 75; ?></b></p>
-<p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?= $npm - 20; ?></b></p>
+<p>Jika aku dikali 5, maka aku sekarang menjadi <b><?= $npm = $npm * 5; ?></b></p>
+<p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?= $npm = $npm / 2; ?></b></p>
+<p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?= $npm = $npm + 75; ?></b></p>
+<p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?= $npm =  $npm - 20; ?></b></p>
